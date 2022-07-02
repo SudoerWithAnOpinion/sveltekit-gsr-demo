8ca052476db7308ac383c0f2e865bec1e2318a04
Samuel Inman <samuel.q.inman@gmail.com>
2022-07-02 09:04:56 -0500

    (feature) Added view for Maintenance Records Table todo: implenent db seeder for test data

---
+++ src/components/Asset/Maintenance/MaintenanceListTable.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { MaintenanceAttributes } from '$models/Assets/Maintenance/Maintenance';
+    import MaintenanceTableRow from './MaintenanceTableRow.svelte';
+    export let maintenances: MaintenanceAttributes[] = [];
+    $: maintenances;
+</script>
+
+<div class="rounded border">
+    <table class="w-full text-left text-gray-500">
+        <thead class="bg-atento-secondary-blue-4 text-atento-secondary-grey-2">
+            <tr>
+                <th scope="col" class="px-6 py-3">ID #</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each maintenances as maintenance}
+                <MaintenanceTableRow maintenance={maintenance}/>
+            {:else}
+                <tr><td colspan="4">No Maintenance Records</td></tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
---
+++ src/components/Asset/Maintenance/MaintenanceTableRow.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { MaintenanceAttributes } from '$models/Assets/Maintenance/Maintenance';
+    export let maintenance: MaintenanceAttributes;
+</script>
+
+<tr class="odd:bg-white even:bg-atento-secondary-blue-1">
+    <td class="px-2 font-medium text-gray-900 whitespace-nowrap">
+        <a href='./maintenance{maintenance.maintenanceId}'>...{maintenance.maintenanceId.substring(24)}</a>
+    </td>
+</tr>
